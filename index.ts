c9c6837fb6d1ba90d5abb373b0163ff760677ddb
IdesignerSe <hebera75@gmail.com>
2023-10-04 15:20:36 +0200

    Update index.ts
    testing a serve to be tested

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    port: 300,
+    fetch(req) {
+        return new Response('Hello Master Joda IDeS');
+    },
+});
+
+console.log(`Listening on port ${server.port}`);
